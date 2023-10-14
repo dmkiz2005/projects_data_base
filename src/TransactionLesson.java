0da9b2cba99663da5203c448485b79668efee2b4
dmkiz2005 <kizilov.dmitro.ukraine@gmail.com>
2023-10-14 22:44:37 +0300

    transaction

---
+++ src/TransactionLesson.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Savepoint;
+import java.sql.Statement;
+
+public class TransactionLesson {
+    public static void main(String[] args) throws Exception {
+        String userName = "root";
+        String password = "a1c90521";
+        String connectionUrl = "jdbc:mysql://localhost:3306/univers";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+             Statement stat = connection.createStatement()) {
+            connection.setAutoCommit(false);
+            stat.execute("drop table if exists books");
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS Books (id MEDIUMINT NOT NULL AUTO_INCREMENT, name CHAR(30) NOT NULL, dt DATE, PRIMARY KEY (id))");
+
+            stat.executeUpdate("insert into books (name) values ('Inferno')");
+            Savepoint savepoint = connection.setSavepoint();
+            stat.executeUpdate("insert into books (name) values ('DaVinchi Code')");
+            stat.executeUpdate("insert into books (name) values ('Solomon key')");
+
+            connection.rollback(savepoint);
+            connection.commit();
+            connection.releaseSavepoint(savepoint);
+        }
+    }
+}
